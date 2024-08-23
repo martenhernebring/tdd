aa2c045418e9e91d552b93fc7583d07bc5ba6159
Marten Hernebring <marten.hernebring@gmail.com>
2024-08-23 05:36:41 +0200

    Created method; added junit 5 to intellij

---
+++ src/main/java/se/hernebring/stringreduction/Main.java
@@ -0,0 +1,10 @@
+package se.hernebring.stringreduction;
+
+public class Main {
+  public static void main() {
+    System.out.println("Hello world!");
+  }
+
+  public static void StringReduction(String str) {
+  }
+}
---
+++ src/test/java/se/hernebring/stringreduction/StringReductionTest.java
@@ -0,0 +1,11 @@
+package se.hernebring.stringreduction;
+
+import org.junit.jupiter.api.Test;
+
+public class StringReductionTest {
+
+  @Test
+  void mainHasMethod() {
+    Main.StringReduction(null);
+  }
+}
